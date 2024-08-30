c0e4ea96e822415e570dd58d4d92d7d2eb03cf37
evan <ereznice@gmail.com>
2024-08-30 18:57:40 +0000

    part way through stack problems

--- src/main/java/com/mycompany/app/neetcode/StackSolutions.java
+++ src/main/java/com/mycompany/app/neetcode/StackSolutions.java
@@ -1,6 +1,8 @@
 package com.mycompany.app.neetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Stack;
 
 public class StackSolutions {
@@ -21,5 +23,57 @@ public class StackSolutions {
             }
         }
         return stackStruct.isEmpty();
+    }
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String c : tokens) {
+            if (c.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (c.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b - a);
+            } else if (c.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (c.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push((int) ((double) b / a));
+            } else {
+                stack.push(Integer.parseInt(c));
+            }
+        }
+        return stack.pop();
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        var res = new ArrayList<String>();
+        var stack = new Stack<Character>();
+
+        parenBacktrack(n, 0, 0, stack, res);
+
+        return res;
+    }
+    
+    private static void parenBacktrack(int n, int openN, int closedN, Stack<Character> stack, ArrayList<String> res) {
+        if (openN == closedN && openN == n) {
+            var parenString = new StringBuilder();
+            for (char c : stack) {
+                parenString.append(c);
+            }
+            res.add(parenString.toString());
+        }
+
+        if (openN < n) {
+            stack.add('(');
+            parenBacktrack(n, openN +1, closedN, stack, res);
+            stack.pop();
+        }
+
+        if (closedN < openN) {
+            stack.add(')');
+            parenBacktrack(n, openN, closedN +1, stack, res);
+            stack.pop();
+        }
     }
 }
--- src/test/java/com/mycompany/app/neetcode/StackSolutionsTest.java
+++ src/test/java/com/mycompany/app/neetcode/StackSolutionsTest.java
@@ -1,7 +1,11 @@
 package com.mycompany.app.neetcode;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +15,15 @@ public class StackSolutionsTest {
         assertTrue(StackSolutions.isValid("[]"));
         assertTrue(StackSolutions.isValid("([{}])"));
         assertFalse(StackSolutions.isValid("[(])"));
+    }
+    @Test
+    public void testEvalRPN() {
+        assertEquals(5, StackSolutions.evalRPN(new String [] {"1","2","+","3","*","4","-"}));
+        assertEquals(6, StackSolutions.evalRPN(new String [] {"4","13","5","/","+"}));
+    }
+
+    @Test
+    public void testGenerateParenthesis() {
+        assertEquals(new ArrayList<String>(Arrays.asList("((()))","(()())","(())()","()(())","()()()")), StackSolutions.generateParenthesis(3));
     }
 }
